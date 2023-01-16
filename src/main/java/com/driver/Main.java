81392798c64dbc48a8043144f06ed48439d6f58e
SHRISHTY MALVIYYA <malviya.shrishty@gmail.com>
2023-01-16 21:13:22 +0530

    Made Product an internal class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,17 @@
 package com.driver;
 
 public class Main {
+    static class Product{
+        public int product(int x, int y){
+            return x*y;
+        }
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
     public static void main(String[] args) {
         Product p = new Product();
         System.out.println(p.product(3,4));
@@ -9,15 +20,4 @@ public class Main {
 
     }
 }
-class Product{
-    public int product(int x, int y){
-        return x*y;
-    }
-    public int product(int x, int y, int z){
-        return x*y*z;
-    }
-    public double product(double x, double y){
-        return x*y;
-    }
 
-}
